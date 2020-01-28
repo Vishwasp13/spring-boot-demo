e4d466f6578a41da206af4e445dee3828771397c
Vishwas <Vishwas.p13@gmail.com>
2020-01-29 00:55:26 +0530

    first commit

---
+++ src/main/java/com/gcpdemo/GcpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gcpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcpdemo/GcpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
